cd2e65e2241b5f73cc538ae77bb06cdaf8425752
paolanovick <paola.novick@davinci.edu.ar>
2026-01-21 11:30:48 -0300

    ajustes

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import { categorias } from "../data/categorias";
+import { categorias } from "../../data/categorias";
 
 const Footer = () => {
   return (
