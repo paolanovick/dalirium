966545b64dfe6fdcde49e6f4736449ac06de5432
paolanovick <paola.novick@davinci.edu.ar>
2026-01-26 18:48:48 -0300

    scroll top

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <BrowserRouter>
+      <ScrollToTop />
+      <AppRoutes />
+    </BrowserRouter>
+  );
 }
 
 export default App;
